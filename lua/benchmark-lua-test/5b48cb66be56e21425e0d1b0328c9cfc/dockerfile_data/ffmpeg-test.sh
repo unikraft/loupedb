b9910efbc48960b75a468c7f7e2efe9eae71c016
Vlad Schiller <vlad.schiller123@gmail.com>
2022-11-16 11:28:53 +0000

    test commit

--- lua/benchmark-lua-test/5b48cb66be56e21425e0d1b0328c9cfc/dockerfile_data/ffmpeg-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-res=$(ffmpeg -formats)
-
-if [[ $res =~ "" ]]; then
-    exit 1
-fi
-
-exit 0
--- lua/benchmark-lua-test/5b48cb66be56e21425e0d1b0328c9cfc/dockerfile_data/jdk-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-res=$(/root/osv-apps/openjdk8-zulu-full/install/usr/lib/jvm/java/jre/bin/java Test)
-
-if [[ $res =~ "test" ]]; then
-    exit 0
-fi
-
-exit 1
